e97f88d4e7de9d690e9b11b7b44ba0880605ab61
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:03:57 +0300

    feat: add containerized services and module wiring

--- apps/api/src/repositories/repository.module.ts
+++ apps/api/src/repositories/repository.module.ts
@@ -1,1 +1,6 @@
-import{Global,Module}from"@nestjs/common";import{MemoryRepository}from"./memory.repository";import{TaskRepository}from"./task.repository";@Global()@Module({providers:[MemoryRepository,TaskRepository],exports:[MemoryRepository,TaskRepository]})export class RepositoryModule{}
+import{Global,Module}from"@nestjs/common";
+import{MemoryRepository}from"./memory.repository";
+import{TaskRepository}from"./task.repository";
+@Global()
+@Module({providers:[MemoryRepository,TaskRepository],exports:[MemoryRepository,TaskRepository]})
+export class RepositoryModule{}
