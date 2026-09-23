14fbebded0dc78d697365f6724e5c654aa3cc568
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 12:00:21 +0300

    feat: wire delegated task worker into collaboration

--- apps/api/src/collaboration/collaboration.module.ts
+++ apps/api/src/collaboration/collaboration.module.ts
@@ -5,10 +5,13 @@ import {MessageBusService} from "./message-bus.service";
 import {MessageModule} from "./message.module";
 import {OrganizationModule} from "../organization/organization.module";
 import {ActivityModule} from "../activity/activity.module";
+import {AgentModule} from "../agent/agent.module";
+import {TaskModule} from "../tasks/task.module";
+import {AgentTaskWorker} from "./agent-task.worker";
 @Module({
- imports:[MessageModule,OrganizationModule,ActivityModule],
+ imports:[MessageModule,OrganizationModule,ActivityModule,AgentModule,TaskModule],
  controllers:[CollaborationController],
- providers:[MessageBusService,CollaborationService],
+ providers:[MessageBusService,CollaborationService,AgentTaskWorker],
  exports:[MessageBusService,CollaborationService]
 })
 export class CollaborationModule {}
