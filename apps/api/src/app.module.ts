1106ebbb3a37e78c00239b249b1592d0f99d1962
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:26:42 +0300

    feat: enable global authentication guard

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -18,11 +18,13 @@ import {OrganizationModule} from "./organization/organization.module";
 import {DepartmentModule} from "./organization/department.module";
 import {ProjectModule} from "./projects/project.module";
 import {E2EFlowModule} from "./e2e-flow/e2e-flow.module";
+import {AuthGuard} from "./auth/auth.guard";
+import {APP_GUARD} from "@nestjs/core";
 
 @Module({
  imports:[InfrastructureModule,SecurityModule,AuditModule,TenantModule,AuthModule,RepositoryModule,
  AgentModule,MemoryModule,TaskModule,TaskDependencyModule,ApprovalModule,CollaborationModule,MessageModule,
  SandboxModule,OrganizationModule,DepartmentModule,ProjectModule,E2EFlowModule],
- controllers:[HealthController]
+ controllers:[HealthController],providers:[{provide:APP_GUARD,useClass:AuthGuard}]
 })
 export class AppModule {}
