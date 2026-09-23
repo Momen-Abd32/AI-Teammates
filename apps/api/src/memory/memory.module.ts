c5908b919717948ead7429aa691a3a6a237e8f4e
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:37:21 +0300

    feat: implement semantic memory embeddings

--- apps/api/src/memory/memory.module.ts
+++ apps/api/src/memory/memory.module.ts
@@ -1,6 +1,13 @@
-import{Module}from "@nestjs/common";
-import{MemoryController}from "./memory.controller";
-import{MemoryService}from "./memory.service";
-import{OrganizationModule}from "../organization/organization.module";
-@Module({imports:[OrganizationModule],controllers:[MemoryController],providers:[MemoryService],exports:[MemoryService]})
-export class MemoryModule{}
+import { Module } from "@nestjs/common";
+import { MemoryController } from "./memory.controller";
+import { MemoryService } from "./memory.service";
+import { MemoryEmbeddingService } from "./embedding.service";
+import { OrganizationModule } from "../organization/organization.module";
+
+@Module({
+  imports:[OrganizationModule],
+  controllers:[MemoryController],
+  providers:[MemoryService,MemoryEmbeddingService],
+  exports:[MemoryService],
+})
+export class MemoryModule {}
