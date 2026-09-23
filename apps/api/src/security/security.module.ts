f8053a860fd9b57d0da48956ad96ae65d7cd1976
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 10:49:51 +0300

    feat: add security and runtime foundation

---
+++ apps/api/src/security/security.module.ts
@@ -0,0 +1,6 @@
+import { Global, Module } from "@nestjs/common";
+import { PermissionService } from "./permission.service";
+
+@Global()
+@Module({ providers: [PermissionService], exports: [PermissionService] })
+export class SecurityModule {}
