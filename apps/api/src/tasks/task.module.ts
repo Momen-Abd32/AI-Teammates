e55575590ebc98801d1ee6ec5d2fbd997ace426c
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:24:35 +0300

    feat: register task dependency repository

--- apps/api/src/tasks/task.module.ts
+++ apps/api/src/tasks/task.module.ts
@@ -1,1 +1,6 @@
-import{Module}from"@nestjs/common";import{TaskController}from"./task.controller";import{TaskService}from"./task.service";@Module({controllers:[TaskController],providers:[TaskService],exports:[TaskService]})export class TaskModule{}
+import {Module} from "@nestjs/common";
+import {TaskController} from "./task.controller";
+import {TaskService} from "./task.service";
+import {TaskRepository} from "../repositories/task.repository";
+@Module({controllers:[TaskController],providers:[TaskService,TaskRepository],exports:[TaskService]})
+export class TaskModule {}
