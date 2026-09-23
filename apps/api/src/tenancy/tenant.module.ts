ea955b57d8609a47d0bd8f96d540f31dd618abf1
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 10:56:35 +0300

    feat: add tenancy auth and persistence schema

--- apps/api/src/tenancy/tenant.module.ts
+++ apps/api/src/tenancy/tenant.module.ts
@@ -1,6 +1,4 @@
-import { Global, Module } from "@nestjs/common";
-import { TenantContextService } from "./tenant-context.service";
-
-@Global()
-@Module({ providers: [TenantContextService], exports: [TenantContextService] })
-export class TenantModule {}
+import {Global,Module} from "@nestjs/common";
+import {TenantContextService} from "./tenant-context.service";
+@Global() @Module({providers:[TenantContextService],exports:[TenantContextService]})
+export class TenantModule{}
