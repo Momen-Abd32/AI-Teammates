1110eb2630bc7cb4793ea834aee0538356a2757e
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:11:39 +0300

    feat: add end to end platform flow

--- apps/api/src/approvals/approval.module.ts
+++ apps/api/src/approvals/approval.module.ts
@@ -1,5 +1,1 @@
-import {Module} from "@nestjs/common";
-import {ApprovalController} from "./approval.controller";
-import {ApprovalService} from "./approval.service";
-@Module({controllers:[ApprovalController],providers:[ApprovalService]})
-export class ApprovalModule{}
+import{Module}from"@nestjs/common";import{ApprovalController}from"./approval.controller";import{ApprovalService}from"./approval.service";@Module({controllers:[ApprovalController],providers:[ApprovalService],exports:[ApprovalService]})export class ApprovalModule{}
