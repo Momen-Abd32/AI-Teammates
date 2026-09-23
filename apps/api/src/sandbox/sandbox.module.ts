f8d161cdb122df32933968c857d03ae1dd94b153
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:32:43 +0300

    feat: wire organization ownership into sandbox

--- apps/api/src/sandbox/sandbox.module.ts
+++ apps/api/src/sandbox/sandbox.module.ts
@@ -1,1 +1,6 @@
-import{Module}from"@nestjs/common";import{SandboxService}from"./sandbox.service";import{SandboxController}from"./sandbox.controller";@Module({controllers:[SandboxController],providers:[SandboxService],exports:[SandboxService]})export class SandboxModule{}
+import{Module}from "@nestjs/common";
+import{SandboxService}from "./sandbox.service";
+import{SandboxController}from "./sandbox.controller";
+import{OrganizationModule}from "../organization/organization.module";
+@Module({imports:[OrganizationModule],controllers:[SandboxController],providers:[SandboxService],exports:[SandboxService]})
+export class SandboxModule{}
