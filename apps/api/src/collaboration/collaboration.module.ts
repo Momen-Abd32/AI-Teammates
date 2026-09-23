c05a3fd2713ffa27b07c675d4e7926264ac58d43
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:54:34 +0300

    fix: provide organization and activity services to collaboration

--- apps/api/src/collaboration/collaboration.module.ts
+++ apps/api/src/collaboration/collaboration.module.ts
@@ -3,8 +3,10 @@ import {CollaborationController} from "./collaboration.controller";
 import {CollaborationService} from "./collaboration.service";
 import {MessageBusService} from "./message-bus.service";
 import {MessageModule} from "./message.module";
+import {OrganizationModule} from "../organization/organization.module";
+import {ActivityModule} from "../activity/activity.module";
 @Module({
- imports:[MessageModule],
+ imports:[MessageModule,OrganizationModule,ActivityModule],
  controllers:[CollaborationController],
  providers:[MessageBusService,CollaborationService],
  exports:[MessageBusService,CollaborationService]
