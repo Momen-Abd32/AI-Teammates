7df0396f20ba05518bb04ff76296746be5111ca8
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 10:58:25 +0300

    feat: connect delegated tasks to agent runtime

---
+++ apps/api/src/collaboration/collaboration.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { CollaborationController } from "./collaboration.controller";
+import { CollaborationService } from "./collaboration.service";
+import { MessageBusService } from "./message-bus.service";
+
+@Module({
+  controllers: [CollaborationController],
+  providers: [MessageBusService, CollaborationService],
+  exports: [MessageBusService, CollaborationService],
+})
+export class CollaborationModule {}
