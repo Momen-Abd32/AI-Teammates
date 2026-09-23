ce3a488da013e07e5fefa096408c30e48713aa94
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:09:20 +0300

    feat: add controlled execution sandbox

--- apps/api/src/sandbox/sandbox.module.ts
+++ apps/api/src/sandbox/sandbox.module.ts
@@ -1,1 +1,1 @@
-import{Module}from"@nestjs/common";import{SandboxService}from"./sandbox.service";@Module({providers:[SandboxService],exports:[SandboxService]})export class SandboxModule{}
+import{Module}from"@nestjs/common";import{SandboxService}from"./sandbox.service";import{SandboxController}from"./sandbox.controller";@Module({controllers:[SandboxController],providers:[SandboxService],exports:[SandboxService]})export class SandboxModule{}
