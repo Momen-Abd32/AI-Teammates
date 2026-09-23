5784ed09840c566e86048b4a8bb66f0338d9fb6a
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:10:51 +0300

    feat: add persistent projects and task APIs

---
+++ apps/api/src/projects/project.module.ts
@@ -0,0 +1,1 @@
+import{Module}from"@nestjs/common";import{ProjectController}from"./project.controller";import{ProjectService}from"./project.service";import{ProjectRepository}from"./project.repository";@Module({controllers:[ProjectController],providers:[ProjectService,ProjectRepository]})export class ProjectModule{}
