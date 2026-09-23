f0017e25d35194c6ee97d871ac9676d775bad150
Momen Abd Al-Haq <abdalhaqmomen@gmail.com>
2026-09-23 11:41:59 +0300

    feat: register automatic memory learning service

--- apps/api/src/memory/memory.module.ts
+++ apps/api/src/memory/memory.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { MemoryController } from "./memory.controller";
 import { MemoryService } from "./memory.service";
 import { MemoryEmbeddingService } from "./embedding.service";
+import { MemoryLearningService } from "./memory-learning.service";
 import { OrganizationModule } from "../organization/organization.module";
 
 @Module({
   imports:[OrganizationModule],
   controllers:[MemoryController],
-  providers:[MemoryService,MemoryEmbeddingService],
-  exports:[MemoryService],
+  providers:[MemoryService,MemoryEmbeddingService,MemoryLearningService],
+  exports:[MemoryService,MemoryLearningService],
 })
 export class MemoryModule {}
